bf8164a8949a460270e35984bee6accd083ef8c4
Marat Davedov <hessium56@gmail.com>
2022-06-13 15:09:40 +0300

    fix 2

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,17 +1,17 @@
 $(function() {
     let header = $('.header');
-    let hederHeight = header.height(); // вычисляем высоту шапки
+    let hederHeight = header.height(); 
      
     $(window).scroll(function() {
       if($(this).scrollTop() > 1) {
        header.addClass('header__fixed');
        $('body').css({
-          'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+          'paddingTop': hederHeight+'px' 
        });
       } else {
        header.removeClass('header__fixed');
        $('body').css({
-        'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+        'paddingTop': 0 
        })
       }
     });
